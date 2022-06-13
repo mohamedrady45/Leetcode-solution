bbb4efbd368a8bcae89f037edaa646bca683e60b
mohamedrady45 <73276555+mohamedrady45@users.noreply.github.com>
2022-06-13 16:11:02 +0200

    Time: 11 ms (37.61%), Space: 9.1 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    int solve (int rows,int col,vector<vector<int>>& triangle)
+    {
+        int n=triangle.size();
+        if (rows==n-1)
+            return triangle[rows][col];
+        if (dp[rows][col]!=-1)
+            return dp[rows][col];
+        int down=solve(rows+1,col,triangle)+triangle[rows][col];
+        int down_right=solve(rows+1,col+1,triangle)+triangle[rows][col];
+        return dp[rows][col]=min(down_right,down);
+        
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-       vector<int>dp(triangle.size(),0);
-        for (int i=0;i<triangle.size();i++)
-        {
-            dp[i]=triangle[triangle.size()-1][i];
-        }
-        for (int i=triangle.size()-2;i>=0;i--)
-        {
-            for (int j=0;j<triangle[i].size();j++)
-            {
-                dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
-            }
-        }
-        return dp.front();
+        dp.resize(triangle.size(),vector<int>(triangle.size(),-1));
+       int res;
+        res=solve(0,0,triangle);
+        return res;
     }
 };
