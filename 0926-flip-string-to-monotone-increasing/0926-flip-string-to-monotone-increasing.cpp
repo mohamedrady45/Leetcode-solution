210425ccaab566d3409b653e03bd7679e0b16071
Mohamed Rady <73276555+mohamedrady45@users.noreply.github.com>
2023-01-17 09:44:20 +0200

    Time: 60 ms (39.75%), Space: 19.2 MB (30.38%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
-    int minFlipsMonoIncr(string s) {
-       int  ones=0 , flip = 0;
-       int ans = 0;
-        for (auto &i : s){
-            if (i=='0'){
-                flip++;
-                flip = min(ones,flip);
-            }
-            else ones++;
+    string s ;
+    int dp[100001][2];
+    int solve (int i , int lst){
+        if (i>=s.size())
+            return 1;
+        int &ret = dp[i][lst];
+        if (~ret)
+            return ret;
+        if (lst==1){
+            if (s[i]=='1')
+             ret = solve(i+1,lst);
+            else ret = solve(i+1,lst)+1;
         }
-        return flip;
+        else {
+            int op1,op2;
+            op1=op2=INT_MAX;
+            if (s[i]=='1'){
+                op1= solve (i+1,1);
+                op2= solve (i+1,0)+1;
+            }
+            else {
+                op1= solve (i+1,1)+1;
+                op2= solve (i+1,0);
+            }
+            ret = min(op1,op2);
+        }
+        return ret;
+    }
+    int minFlipsMonoIncr(string ss) {
+      s = ss;
+      memset(dp,-1,sizeof dp);
+      int ans = solve (0,0);
+      return --ans;
     }
 };
