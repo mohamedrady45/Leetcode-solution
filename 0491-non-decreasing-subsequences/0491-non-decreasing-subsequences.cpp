ab3617c3dea4abaad2c17c20b59925333a9b6b17
Mohamed Rady <73276555+mohamedrady45@users.noreply.github.com>
2023-01-20 10:46:20 +0200

    Time: 514 ms (6.57%), Space: 110 MB (5.26%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+       set<vector<int>>st;
+        int n = nums.size();
+        for (int i=0;i<(1<<n);i++){
+            vector<int>curr;
+            for (int j=0;j<n;j++){
+               if (i&(1<<j))
+                   curr.push_back(nums[j]);
+            }
+            if(curr.size()>=2 && is_sorted(curr.begin(),curr.end()))
+            st.insert(curr);
+        }
+         vector<vector<int>>ans;
+         for (auto &i : st)
+             ans.push_back(i);
+        return ans;
+    }
+};
