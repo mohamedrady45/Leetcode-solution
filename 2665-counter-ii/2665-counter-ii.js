b73d83e0e58bd323f7c93162febdae67217b914c
Mohamed Rady <73276555+mohamedrady45@users.noreply.github.com>
2023-09-01 17:35:11 +0300

    Time: 64 ms (45.21%), Space: 44.4 MB (66.18%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let x = init;
+    function increment () {
+        return ++x;
+    }
+     function decrement () {
+        return --x;
+    }
+     function reset () {
+        return x = init;
+    }
+    return {increment , decrement , reset};
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
